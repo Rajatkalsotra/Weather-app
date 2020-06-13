6670ddec93dbd295b5c6ac21b0ad443659c1541b
RajatKalsotra <rajat3468.rk@gmail.com>
2020-06-13 23:43:06 +0530

    Initial files

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+Nimport 'package:flutter/material.dart';
+import './kilematic.dart';
+
+void main() {
+   runApp(
+      new MaterialApp(
+         title: 'Weather',
+         home: new Klimatic(),
+      )
+   );
+}
---
+++ lib/utils.dart
@@ -0,0 +1,2 @@
+final defaultCity = "JAMMU";
+var appId='c68b1f30bbabc418f4f60a56969ac472';
